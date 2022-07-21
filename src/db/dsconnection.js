a4d3be7e6ac51baefe5318b86f52fd61b0ab31bd
Ayush <jainboy1468@gmail.com>
2022-07-21 11:17:34 +0530

    dd

--- src/db/dsconnection.js
+++ src/db/dsconnection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://Ayush:ayushjain@cluster0.8wcst.mongodb.net/Images?retryWrites=true&w=majority" ,
+mongoose.connect("mongodb+srv://Ayush:ayushjain@cluster0.8wcst.mongodb.net/?retryWrites=true&w=majority" ,
 { useNewUrlParser: true,
 useUnifiedTopology: true,
 
